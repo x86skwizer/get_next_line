f91b74371fc75a767cd4cbb5b87569a5165ee9a6
Yassine Amrire <yamrire@e2r13p9.1337.ma>
2022-03-16 00:13:32 +0100

    gnl

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -19,11 +19,10 @@
 # include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 50
 # endif
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
