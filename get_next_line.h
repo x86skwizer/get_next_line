dc3884d05531ec984b0a3f268e10d0e38d543620
Yassine Amrire <yamrire@e2r13p9.1337.ma>
2022-03-16 00:13:06 +0100

    gnl

--- get_next_line.h
+++ get_next_line.h
@@ -22,7 +22,6 @@
 # endif
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *src);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
